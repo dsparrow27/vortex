049dd799590b360f19fa660b8484ab483a064878
dave <dsparrow27@gmail.com>
2016-01-09 22:33:43 +1100

    small change

--- src/ds/vortex/customLogger.py
+++ src/ds/vortex/customLogger.py
@@ -7,13 +7,9 @@ import sys
 def getCustomLogger(name='vortex', loglevel='INFO'):
     log = logging.getLogger(name)
 
-    # if logger 'name' already exists, return it to avoid logging duplicate
-    # messages by attaching multiple handlers of the same type
     if log.handlers:
         return log
-    # if logger 'name' does not already exist, create it and attach handlers
     else:
-        # set logLevel to loglevel or to INFO if requested level is incorrect
         loglevel = getattr(logging, loglevel.upper(), logging.INFO)
         log.setLevel(loglevel)
 

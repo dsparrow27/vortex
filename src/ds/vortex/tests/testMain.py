93eff6e0b20843616e9c41b80702a2c645725bd2
dave <dsparrow27@gmail.com>
2016-01-11 09:56:23 +1100

    added checkstate

--- src/ds/vortex/tests/testMain.py
+++ src/ds/vortex/tests/testMain.py
@@ -2,12 +2,8 @@ import unittest
 
 
 def createTestSuites():
-<<<<<<< Updated upstream
-    tests = ["testEdge", "testGraph", "testBaseNodes", "testBasicMathNodes", "testPlug", "testEventHandler"]
-=======
     tests = ["testEdge", "testGraph", "testBaseNodes", "testBasicMathNodes", "testPlug", "testTrigonometry",
              "testConversion", "testDict", "testString", "testArray", "testComparison", "testConstants"]
->>>>>>> Stashed changes
 
     suites = [unittest.defaultTestLoader.loadTestsFromName("ds.vortex.tests."+name) for name in tests]
     testSuite = unittest.TestSuite(suites)

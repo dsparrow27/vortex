04bb680eae2f5176e9a568baf2017cee3e676365
dave <dsparrow27@gmail.com>
2016-02-04 20:52:44 +1100

    started matrix

--- src/ds/vortex/utils/matrix.py
+++ src/ds/vortex/utils/matrix.py
@@ -3,16 +3,24 @@ class Matrix(object):
         self.matrix = matrix
 
     def __repr__(self):
-        pass
+        return "{}{}".format(self.__class__.__name__, self.matrix)
 
-    def __getitem__(self, item):
-        pass
+    def __eq__(self, other):
+        return isinstance(other, self) and self.matrix == other.matrix
 
-    def __setitem__(self, key, value):
+    def __getitem__(self, row, column):
+        return self.matrix[row][column]
+
+    def __setitem__(self, row, column, value):
+        self.matrix[row][column] = value
+
+    def __mul__(self, other):
+        pass
+    def __sub__(self, other):
         pass
 
     def clear(self):
-        pass
+        self.matrix = [[] for i in xrange(len(self.matrix))]
 
     def translate(self):
         pass

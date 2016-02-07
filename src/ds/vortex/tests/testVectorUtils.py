65d23265914ab4201ee84e5fcd51d6a38d885470
dave <dsparrow27@gmail.com>
2016-02-08 07:58:41 +1100

    merge of cleanup branch and small test fix

--- src/ds/vortex/tests/testVectorUtils.py
+++ src/ds/vortex/tests/testVectorUtils.py
@@ -4,8 +4,8 @@ from ds.vortex.utils import vector
 
 class Test2DVector(unittest.TestCase):
     def setUp(self):
-        self.vector = vector.Vector2D([2.0, 3.0])
-        self.vec = vector.Vector2D([2.0, 3.0])
+        self.vector = vector.Vector([2.0, 3.0])
+        self.vec = vector.Vector([2.0, 3.0])
 
     def testLength(self):
         self.assertEquals(len(self.vector), 2)
@@ -13,30 +13,30 @@ class Test2DVector(unittest.TestCase):
         self.assertEquals(self.vector.length(), 3.605551275463989)
 
     def testNormalize(self):
-        self.assertEquals(self.vector.normalize(), vector.Vector2D([0.5547001962252291, 0.8320502943378437]))
+        self.assertEquals(self.vector.normalize(), vector.Vector([0.5547001962252291, 0.8320502943378437]))
 
     def testEquals(self):
         self.assertTrue(self.vec, self.vector)
         self.assertNotEquals([2, 6], self.vector)
-        self.assertNotEquals(vector.Vector2D([2.0, 3.5]), self.vector)
-        vector2 = vector.Vector2D(self.vec)
-        self.assertEquals(vector2.vec, [2.0, 3.0])
-        self.assertEquals(vector2 + vector2, vector.Vector2D([4.0, 6.0]))
+        self.assertNotEquals(vector.Vector([2.0, 3.5]), self.vector)
+        vector2 = vector.Vector(self.vec)
+        self.assertEquals(vector2.vec, vector.Vector[2.0, 3.0])
+        self.assertEquals(vector2 + vector2, vector.Vector([4.0, 6.0]))
 
     def testAdd(self):
         added = self.vec + self.vector
-        self.assertEquals(added, vector.Vector2D([4.0, 6.0]))
+        self.assertEquals(added, vector.Vector([4.0, 6.0]))
         self.vec += self.vector
-        self.assertEquals(added, vector.Vector2D([4.0, 6.0]))
+        self.assertEquals(added, vector.Vector([4.0, 6.0]))
 
     def testSubtract(self):
-        self.assertEquals(self.vector - self.vec, vector.Vector2D([0.0, 0.0]))
+        self.assertEquals(self.vector - self.vec, vector.Vector([0.0, 0.0]))
         self.vec.x = -10
         self.vec.y = -5
 
     def testNeg(self):
-        self.assertNotEquals(-self.vector, vector.Vector2D([-2.0, 3.0]))
-        self.assertEquals(-self.vector, vector.Vector2D([-2.0, -3.0]))
+        self.assertNotEquals(-self.vector, vector.Vector([-2.0, 3.0]))
+        self.assertEquals(-self.vector, vector.Vector([-2.0, -3.0]))
 
     def testGetX(self):
         self.assertEquals(self.vector[0], 2.0)
@@ -49,4 +49,4 @@ class Test2DVector(unittest.TestCase):
     def testMult(self):
         self.assertEquals(self.vector * self.vec, 13)
         self.assertEquals(self.vector * -self.vec, -13)
-        self.assertEquals(10 * self.vector, vector.Vector2D([20.0, 30.0]))
+        self.assertEquals(10 * self.vector, vector.Vector([20.0, 30.0]))
